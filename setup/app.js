ba41bf15813e1ed5f27e6bfdef37893bb8fdb5a3
Genn <genesismaldonadoaugusto@gmail.com>
2023-04-24 00:33:29 -0400

    Started css styling

--- setup/app.js
+++ setup/app.js
@@ -30,12 +30,54 @@ navToggle.addEventListener('click', function(){
 
 // ********** fixed navbar ************
 const navbar = document.getElementById('nav');
-const topLink = document.querySelector('.top-link')
+const topLink = document.querySelector('.top-link');
 window.addEventListener('scroll', function(){
-    console.log(window.pageYOffset);
+    const scrollHeight = window.pageYOffset;
+    const navHeight = navbar.getBoundingClientRect().height;
+    if(scrollHeight > navHeight){
+        navbar.classList.add('fixed-nav');
+    }
+    else{
+        navbar.classList.remove('fixed-nav');
+    }
 
+    if(scrollHeight > 500){
+        topLink.classList.add('show-link');
+    }
+    else{
+        topLink.classList.remove('show-link');
+    }
 
 })
 
 // ********** smooth scroll ************
+const scrollLinks =  document.querySelectorAll('.scroll-link');
+
+scrollLinks.forEach(function (link) {
+    link.addEventListener('click', function(e){
+        // prevent default
+        e.preventDefault();
+        // navigate to specific section
+        const id = e.currentTarget.getAttribute('href').slice(1);
+        const element = document.getElementById(id);
+// calculate the heights
+const navHeight = navbar.getBoundingClientRect().height;
+const containerHeight = linksContainer.getBoundingClientRect().height;
+const fixedNav = navbar.classList.contains('fixed-nav');
+
+        let position = element.offsetTop - navHeight;
+
+        if(!fixedNav){
+            position = position - navHeight;
+        }
+        if (navHeight > 82){
+            position = position + containerHeight;
+        }
+        window.scrollTo({
+            left: 0,
+            top: position,
+        });
+        linksContainer.style.height = 0;
+    });
+});
 // select links
